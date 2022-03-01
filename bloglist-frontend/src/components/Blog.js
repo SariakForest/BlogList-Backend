acd668feae2418458a653c9abb0a668f512365ba
DarioMeliaBasus <dario@meliabasus.com>
2022-03-01 14:38:27 +0100

    5.7 solved

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from "react"
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} <i>By</i> {blog.author}
-  </div>
-)
+import { useState } from "react"
+
+import Button from "./Button"
+const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  const expandBlog = () => setShowInfo(!showInfo)
+
+  const infoStyle = {
+    padding:20,
+    width:"fit-content"
+  }
+  const blogInfo = (
+    <div style={infoStyle}>
+      <hr></hr>
+      <p><a href={blog.url}>Link to blog</a><br></br></p>
+      <p>likes {blog.likes} <Button text="👍" /></p>
+      <p>{blog.author}</p>
+      <hr></hr>
+    </div>
+  )
+
+  return (
+    <div>
+      {blog.title}{" "}
+      <Button onClick={expandBlog} text={showInfo ? "hide" : "show"} />
+      {showInfo ? blogInfo : ""}
+    </div>
+  )
+}
 
 export default Blog
